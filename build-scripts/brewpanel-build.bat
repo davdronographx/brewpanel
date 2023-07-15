a00591c4dda17990c5f78d82f835ce5a46f0754e
Sam Davenport <sam@davenportdronographics.com>
2023-07-14 19:30:09 -0500

    added types and debug info

--- build-scripts/brewpanel-build.bat
+++ build-scripts/brewpanel-build.bat
@@ -1,5 +1,5 @@
 @echo off
 
-cl.exe /w /I dependencies /I src\\ /LD /Zi src\\brewpanel.cpp /Fd: bin\\brewpanel.pdb /Fo: bin\\brewpanel.obj /Fe: bin\\brewpanel.dll /link user32.lib gdi32.lib opengl32.lib /DEBUG:FULL
+cl.exe /I dependencies /I src\\ /Zi /Fe: bin\\brewpanel.exe src\\brewpanel.cpp /Fd: bin\\brewpanel.pdb /Fo: bin\\brewpanel.obj /link user32.lib  /DEBUG:FULL
 
 @REM del bin\*.raf
---
+++ src/brewpanel.cpp
@@ -0,0 +1,15 @@
+#include <Windows.h>
+
+#include "brewpanel-types.hpp"
+
+s32 WINAPI 
+wWinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    PWSTR pCmdLine,
+    s32 nCmdShow) {
+    
+    
+
+    return 0;
+}
