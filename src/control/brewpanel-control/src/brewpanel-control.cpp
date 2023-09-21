2fe3f60675c6611b70df860be5219fc6c42e9594
davdronographx <91574297+davdronographx@users.noreply.github.com>
2023-09-20 19:09:14 -0500

    starting the FUCK over with the control code

--- src/control/brewpanel-control/src/brewpanel-control.cpp
+++ src/control/brewpanel-control/src/brewpanel-control.cpp
@@ -1,60 +1,19 @@
 #include <Arduino.h>
-#include <avr8-stub.h>
-#include <app_api.h>
-#include "brewpanel-control.hpp"
-#include "brewpanel-control-communication.cpp"
-#include "brewpanel-control-temperature.cpp"
-
-global BrewpanelControlState control  = {0};
 
 void setup() {
-    control.comm = brewpanel_control_communication_init();
-    control.temp = brewpanel_control_temperature_init();
+    Serial.begin(9600,SERIAL_8N1);
+    Serial.setTimeout(1000);
 }
 
-void loop() {
+void loop() {;
+
+    Serial.write("test");
+
+    delay(250);
+}
+
+void serialEvent() {
 
 
-    // brewpanel_control_temperature_update(&control.temp);
-
-    // brewpanel_control_communication_update(
-    //     &control.comm,
-    //     control.temp
-    // );
-
-    BufferTest test = {0};
-    test.heartbeat.boil_element_temp = 111;
-    test.heartbeat.hlt_element_temp = 222;
-    test.heartbeat.mlt_element_temp = 222;
-
-    Serial.write(test.buffer,sizeof(test.heartbeat));
-
-    delay(1000);
-
-    // delay(1000);
-    // Serial.write("HELLO");
-
-    // mem_byte read_buffer[BREWPANEL_COMMUNICATION_MESSAGE_BUFFER_SIZE];
-    // u32 bytes_read = 0;
-
-    // while (Serial.available()) {
-    //     delay(10);
-        
-    //     if (Serial.available() > 0) {
-            
-    //         char c = Serial.read();
-    //         read_buffer[bytes_read] = c;
-    //         ++bytes_read;
-
-    //         if (c == '\0') {
-    //             break;
-    //         }
-    //     }
-    // }
-
-    // if (bytes_read > 0) {
-    //     Serial.write("ASDF");
-    //     delay(100);
-    // }
 
 }
